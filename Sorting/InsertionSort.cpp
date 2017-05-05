b6224ceeaf32692de9e74c2174af394fc77b8773
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2017-05-05 18:20:58 +0530

    Create InsertionSort.cpp

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,26 @@
+/*
+Insertion Sort
+--------------
+A simple sorting algorithm that builds a final sorted list by
+placing individual elements at their relative ordered places
+one at a time. Most efficient O(n^2) time sorting algorithm.
+Efficient for data sets that are already substantially sorted,
+the time complexity is O(nk) when each element in the input is 
+no more than k places away from its sorted position.
+--------------
+Time Complexity: Best Case: O(n)
+                 Average and Worst Case: O(n^2)
+Space Complexity: O(1)
+Stable: Yes
+In-Place: Yes
+Online: Yes
+*/
+
+#include <iosteam>
+#include <vector>
+
+using namespace std;
+
+void insertion_sort(vector<int> v, int order){
+    
+}
