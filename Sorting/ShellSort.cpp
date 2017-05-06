914569b66fbb7197819ac730e15318424f7c59a0
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2017-05-07 04:02:44 +0530

    Update ShellSort.cpp

--- Sorting/ShellSort.cpp
+++ Sorting/ShellSort.cpp
@@ -12,7 +12,7 @@ determining their time complexity remains an open problem.
 The original 1959 algorithm is implemented here (using shell sequence).
 Use order = +/- 1 for increasing and decreasing orders respectively.
 ----------
-Time Complexity : Best Case : O(n*logn)
+Time Complexity : Best Case : O(n*logn^2)
                   Average and Worst Case : O(n^2)
 Space Complexity : O(1)
 Stable : No
@@ -20,3 +20,20 @@ In-Place : Yes
 Online : No
 */
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void shell_sort(vector<int> v, int order){
+    for (size_t gap = v.size() / 2; gap > 0; gap /= 2){
+        for (size_t i = gap; i < v.size(); i++){
+            int element = v[i];
+            size_t j;
+            for (j = i; j >= gap && order * v[j - gap] > element; j -= gap){
+                v[j] = v[j - gap];
+            }
+            v[j] = element;
+        }
+    }
+}
