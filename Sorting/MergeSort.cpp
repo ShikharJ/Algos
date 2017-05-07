56e9ea0a7a817ffc1d2ffdd657a4499bb459c679
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2017-05-07 13:15:35 +0530

    Create MergeSort.cpp

---
+++ Sorting/MergeSort.cpp
@@ -0,0 +1,23 @@
+/*
+Merge Sort
+----------
+An efficient, comparison-based, divide and conquer sorting algorithm.
+It divides input array into two halves, recursively calls itself for 
+the two halves and then merges the two sorted halves.
+Use order = +/- 1 for increasing and decreasing orders respectively.
+----------
+Time Complexity : Best, Average and Worst Case : O(n*logn)
+Space Complexity : O(n)
+Stable : Yes
+In-Place : No
+Online : No
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge_sort(vector<int> v, size_t start, size_t end, int order){
+    
+}
