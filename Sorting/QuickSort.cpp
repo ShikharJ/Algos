c36a21673a798c49b2258bee140f1a48352e69eb
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2017-05-13 03:19:29 +0530

    Create QuickSort.cpp

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,17 @@
+/*
+Quick Sort
+----------
+Quicksort or Partition-Exchange Sort is an efficient sorting algorithm, serving as a systematic method 
+for placing the elements of an array in order. Developed by Tony Hoare (ACM Turing Award Recipient), 
+it is still a commonly used algorithm for sorting. When implemented well, it can be about two or three 
+times faster than its main competitors, merge sort and heapsort.
+Use order = +/- 1 for increasing and decreasing orders respectively.
+----------
+Time Complexity : Best and Average Case : O(n*logn)
+ 				  Worst Case : O(n^2)
+Space Complexity : O(n)
+Stable : No
+In-Place : Yes
+Online : No
+*/
+
