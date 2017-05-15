c547c13e2554a90e688dd77bc7df54c3c46d3cac
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2017-05-15 23:21:43 +0530

    Updated QuickSort.cpp

--- Sorting/QuickSort.cpp
+++ Sorting/QuickSort.cpp
@@ -15,3 +15,34 @@ In-Place : Yes
 Online : No
 */
 
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+srand(time(NULL));
+
+size_t partition(vector<int> v, size_t start, size_t end, int order){
+	size_t random_index = start + (rand() % (end - start + 1));
+	swap(v[random_index], v[start]);
+	int pivot = v[start];
+	size_t i = start + 1;
+	for (size_t j = start + 1; j <= end; j++){
+		if (order * v[j] < order * v[pivot]){
+			swap(v[i], v[j]);
+			i += 1;
+		}
+	}
+	swap(v[start], v[i - 1]);
+	return i - 1;
+}
+
+void quick_sort(vector<int> v, size_t start, size_t end, int order){
+	if (start < end){
+		size_t pivot_index = partition(v, start, end, order);
+		quick_sort(v, start, pivot_index - 1, order);
+		quick_sort(v, pivot_index + 1, end, order);
+	}
+}
