357e1a4c951bbe0bfa479acc16b91a3babf36c3e
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2017-06-22 05:32:24 +0530

    Update

--- Practical Applications/MaximumWeightIndependentSetReconstruction.cpp
+++ Practical Applications/MaximumWeightIndependentSetReconstruction.cpp
@@ -14,13 +14,16 @@ using namespace std;
 vector<int> wisr(vector<int> a){
 	vector<int> s;
 	size_t iter = a.size() - 1;
-	while (iter >= 0){
-		if (a[iter - 1] >= a[iter - 2]){
+	while (iter > 1){
+		if (a[iter - 1] >= a[iter - 2] + a[iter]){
 			iter--;
 		} else{
 			s.push_back(iter);
 			iter -= 2;
 		}
+	}
+	if (iter == 1){
+		s.push_back(iter);
 	}
 	sort(s.begin(), s.end());
 	return s;
